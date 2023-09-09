759ce57bc8a84bee028ca12bb1b72eb50b690ae7
dakamakat <gazon-ozon@yandex.ru>
2023-09-09 20:32:29 +0200

    feat(clap): basic clap usage add

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+fn main() {
+    let args = Cli::parse();
+
+    println!(
+        "pattern: {0} , path: {1}",
+        args.pattern,
+        args.path.display()
+    );
+}
