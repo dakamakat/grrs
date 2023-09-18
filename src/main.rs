372ec40e19a253f1ac259541387f8dcea894a3d0
dakamakat <gazon-ozon@yandex.ru>
2023-09-18 14:30:51 +0200

    feat(read): add buf and no buf read

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
 
 use clap::Parser;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::time::Instant;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -10,12 +14,52 @@ struct Cli {
     /// The path to the file to read
     path: std::path::PathBuf,
 }
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Cli::parse();
+    let now = Instant::now();
 
-    println!(
-        "pattern: {0} , path: {1}",
-        args.pattern,
-        args.path.display()
-    );
+    // Code block to measure.
+    {
+        println!(
+            "pattern: {0} , path: {1}",
+            args.pattern,
+            args.path.display()
+        );
+
+        read_buf(&args);
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    Ok(())
+}
+
+fn read_no_buf(args: &Cli) {
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
+
+fn read_buf(args: &Cli) -> std::io::Result<()> {
+    let f = File::open(&args.path)?;
+
+    let mut reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        let res = match line {
+            Ok(line) => line,
+            Err(e) => return Err(e),
+        };
+
+        if res.contains(&args.pattern) {
+            println!("{}", res);
+        }
+    }
+
+    Ok(())
 }
